f1659a0d7ce1d85f2f85a4b3e67fc3795dcdf069
RomainViC <romain.viellecarre@gmail.com>
2020-06-03 10:57:16 +0200

    validation update score

--- app/Http/Controllers/ValidationMdpController.php
+++ app/Http/Controllers/ValidationMdpController.php
@@ -17,6 +17,26 @@ class ValidationMdpController extends Controller
 
         if ($riddledb->code == $request->input('code')) {
             end_riddle($riddledb, Auth::user());
+			$format = 'Y-m-d H:i:s';
+            $dateFin = DateTime::createFromFormat($format, DB::table('riddles_teams')->where([
+                ['team_id', '=', $user->id],
+                ['riddle_id', '=', $id],
+            ])->value('end_date'));
+            $dateDebut = DateTime::createFromFormat($format, DB::table('riddles_teams')->where([
+                ['team_id', '=', $user->id],
+                ['riddle_id', '=', $id],
+            ])->value('start_date'));
+            $duration=$dateFin->getTimeStamp()-$dateDebut->getTimestamp();
+            if ($duration<=480){
+                $score=20;
+            } elseif ($duration<=600) {
+                $score=10;
+            }
+            else{
+                $score=-10;
+            }
+            $user->score=$user->score+$score;
+            $user->save();
 
             $this->submitMessage(Auth::user()->rooms->first(),$riddledb,Auth::user());
 
