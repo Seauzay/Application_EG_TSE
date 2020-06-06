df61abb78ac4668f45899b7880088263be8519cc
RomainViC <romain.viellecarre@gmail.com>
2020-06-06 10:09:17 +0200

    add Broadcasting and emoji actualisation

--- resources/views/player/home.blade.php
+++ resources/views/player/home.blade.php
@@ -198,7 +198,5 @@
                 {{--div de base de la grille d'énigmes--}}
         const playerRiddleGrid = new PlayerRiddleGrid('#mySuperRiddleGrid');
         const res = playerRiddleGrid.update();
-
-
     </script>
 @endsection
